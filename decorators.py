ec537237075c4e11d360888735845d940d689f35
kolotyshka <konstantincherbar05@gmail.com>
2025-05-22 10:51:22 +0300

    Add logging decorator

--- decorators.py
+++ decorators.py
@@ -1,1 +1,11 @@
- 
+from datetime import datetime
+import functools
+
+def log_command(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open("bot.log", 'w') as f:
+            f.write(f"{timestamp} Command {func.__name__} called\n")
+        return await func(*args, **kwargs)
+    return wrapper
