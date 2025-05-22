0120b4d8a0fe1dff8280b3fccca9a690433bb7fb
kolotyshka <konstantincherbar05@gmail.com>
2025-05-22 11:14:29 +0300

    Extend OOP structure with methods

--- models.py
+++ models.py
@@ -3,6 +3,9 @@ class Reminder:
         self.text = text
         self.time = time
 
+    def to_dict(self):
+        return {"text": self.text, "time": self.time}
+
 class Bot:
     def __init__(self):
         self.reminders = []
@@ -10,9 +13,15 @@ class Bot:
     async def send_reminder(self, reminder):
         pass
 
+    def add_reminder(self, reminder):
+        self.reminders.append(reminder)
+
 class Storage:
     def __init__(self):
         self.db = None
 
     def save_reminder(self, reminder):
         pass
+
+    def load_reminders(self):
+        return []
