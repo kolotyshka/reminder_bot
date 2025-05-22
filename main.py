b646f254e42f41a0d22c9c6e536d709814b4eca0
kolotyshka <konstantincherbar05@gmail.com>
2025-05-22 11:06:21 +0300

    Add basic bot with /start command

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import asyncio
 
+API_TOKEN = "7936831978:AAHRX2viqIzDhG3IvlvlZjUzD0rUHCub_3o"
 
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def on_start(message: types.Message):
+    await message.reply("Welcome to Reminder Bot!"
+                        " Use /add <text> at <time> to set a reminder")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__"
+    asyncio.run(main())
