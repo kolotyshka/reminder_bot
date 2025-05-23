242a8eb04f8e57528b81aa45e32c35aa2b388b56
kolotyshka <konstantincherbar05@gmail.com>
2025-05-23 17:52:07 +0300

    Add /add command

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import asyncio
+
+from decorators import log_command
+from models import Reminder
 
 API_TOKEN = "7936831978:AAHRX2viqIzDhG3IvlvlZjUzD0rUHCub_3o"
 
@@ -11,6 +15,19 @@ dp = Dispatcher()
 async def on_start(message: types.Message):
     await message.reply("Welcome to Reminder Bot!"
                         " Use /add <text> at <time> to set a reminder")
+
+@dp.message(Command("add"))
+@log_command
+async def on_add(message: types.Message):
+    parts = message.text.split(" at ", 1)
+    if len (parts) != 2:
+        await message.reply("Use: /add <text> at <time>")
+        return
+    text, time = parts
+    reminder = Reminder(text, time)
+    Bot().add_reminder(reminder)
+    await message.reply
+    await message.reply("Reminder set!")
 
 async def main():
     await dp.start_polling(bot)
